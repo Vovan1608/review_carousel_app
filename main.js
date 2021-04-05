35e8360c654486b7bfb5f46e88fe022147d86620
Vovan1608 <vovan1608@gmail.com>
2021-04-05 16:46:45 +0300

    added obj men to main.js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const mens = [
+	{
+		id: 1,
+		name: "vladymyr zenin",
+		job: "web developer",
+		img: "./img/person1.jpg",
+		text:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quidem repellat tenetur. Quia id dolores quod exercitationem officia beatae ad?",
+	},
+	{
+		id: 2,
+		name: "vasya popkin",
+		job: "back-end developer",
+		img: "./img/person2.jpg",
+		text: 
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quidem repellat tenetur. Quia id dolores quod exercitationem officia beatae ad?",
+	},
+	{
+		id: 3,
+		name: "bob bobbovich",
+		job: "ui designer",
+		img: "./img/person3.jpg",
+		text:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quidem repellat tenetur. Quia id dolores quod exercitationem officia beatae ad?"",
+	}
+]
